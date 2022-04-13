9b04293a5d8d056fb17b8411d71699fb3df72f7a
DidItWork <ben456jamin7891@gmail.com>
2022-04-14 00:30:15 +0800

    Door opening and closing implemented

--- arduino_env/Input/Input_Program.ino
+++ arduino_env/Input/Input_Program.ino
@@ -10,6 +10,42 @@ float avg_size = 20.0; // amount of averages for each mass measurement
 
 Q2HX711 hx711(hx711_data_pin, hx711_clock_pin); // prep hx711
 
+//Motor Connections
+int enA = 9;
+int in1 = 8;
+int in2 = 7;
+int door = 6;
+
+
+class Input_State_Machine
+{
+
+  public:
+
+  String state;
+  String ns;
+  String output;
+  String start_state = "idle";
+
+  void startMachine(){
+    state = start_state;
+    return;
+  }
+
+  void get_next_values(String state, String inp){
+    if (state=="idle" && inp=="open"){
+      
+    }
+    return;
+  }
+  
+  String step_function(String inp){
+    get_next_values(state,inp);
+    state = ns;
+    return output;
+  }
+};
+
 void setup() {
   Serial.begin(9600); // prepare serial port
   delay(1000); // allow load cell and hx711 to settle
@@ -21,6 +57,17 @@ void setup() {
   }
   x0/=long(avg_size);
   int ii = 1; 
+
+  pinMode(enA, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+
+  digitalWrite(in1,LOW);
+  digitalWrite(in2,LOW);
+
+  openDoor(50);
+  delay(1000);
+  closeDoor(40);
 }
 
 bool mass_reading(){
@@ -39,7 +86,33 @@ bool mass_reading(){
   }
 }
 
+void openDoor(int m_speed){
+  Serial.print("Motors Moving... Speed:");
+  Serial.println(m_speed);
+  analogWrite(enA, m_speed);
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  delay(5000);
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+}
+
+void closeDoor(int m_speed){
+  analogWrite(enA, m_speed);
+  Serial.println("Door Closing...");
+  while(digitalRead(door)){
+    digitalWrite(in1, HIGH);
+    digitalWrite(in2, LOW);
+  }
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+}
+
 void loop() {
   bool valid_input = mass_reading();
   Serial.println("Valid Input: "+String(valid_input));
+
+  //Read Serial and wait for pedal press
+
+  
 }
