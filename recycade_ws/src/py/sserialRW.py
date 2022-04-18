3a61877e9bd7d4fcea0619bb2ab16bb1e9145248
Ndrusr <tnwz99@gmail.com>
2022-04-18 20:16:28 +0800

    pythonn

--- recycade_ws/src/py/sserialRW.py
+++ recycade_ws/src/py/sserialRW.py
@@ -3,24 +3,36 @@ import serial
 serUNO = serial.Serial('/dev/ttyUSB1', 9600)
 serMega = serial.Serial('/dev/ttyUSB0', 9600)
 
-megaRead = bytes(7)
-unoRead = bytes(7)
+
 
 actionmsg = "ZA00000\n"
 pushmsg = "ZB00000\n"
 acceptmsg = "Zaccept"
 rejectmsg = "Zreject"
 
+print("Cycle begin")
+
 while(True):
-    print("Cycle begin")
+    
     megaRead = serMega.readline()
 
     if(megaRead == "ZA00000"):
+        print("serial get!")
         serUNO.write(actionmsg)
-        while(not unoRead[0] == 'Z'):
+        while(not unoRead[0] == 'Z' and len(unoRead) == 7):
             unoRead = serUNO.readline()
-        if()
-        serMega.write(pushmsg)
+            if(not unoRead[0] == 'Z'):
+                print(unoRead)
+            elif(len(unoRead) == 7 ):
+                print("signal get")
+                if(unoRead == acceptmsg):
+                    serMega.write(pushmsg)
+                    print('accepting')
+                    break
+                else:
+                    print('rejected')
+                    break
+        
 
 
 
